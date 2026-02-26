75999f0c4c3e7c9a4f8e062b9bd4f875309b7bf5
Krishna <destructorboys23@gmail.com>
2026-02-26 11:50:13 +0530

    feat: initialized the spring boot project

---
+++ src/test/java/com/clinicflow/backend/ClinicflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicflow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
